c7008e4338f775a2e6307e1831c1af7749c2768d
coder-nik200 <codesnippet17@gmail.com>
2026-01-29 14:05:22 +0530

    Add some more data in Role Baded Chatbot

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-// const getGeminiResponse = require("../controllers/openAiController");
 const getGeminiResponse = require("../controllers/geminiController");
+const { getMessage } = require("../controllers/chatController");
+const getGeminiResponse2 = require("../ChatBot/Gemini-3");
 
 const router = express.Router();
 
-// router.post("/chat", getMessage);
+router.post("/chat", getMessage);
 router.post("/chat", getGeminiResponse);
+router.post("/chat", getGeminiResponse2);
 
 module.exports = router;
